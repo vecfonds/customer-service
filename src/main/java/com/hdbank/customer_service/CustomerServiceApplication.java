212eadd268392144ab1a27d27a8d67137209afe4
vecfonds <dungsppkhdallss@gmail.com>
2025-04-10 08:23:20 +0700

    feat(API Gateway): configure Eureka Client

--- src/main/java/com/hdbank/customer_service/CustomerServiceApplication.java
+++ src/main/java/com/hdbank/customer_service/CustomerServiceApplication.java
@@ -2,8 +2,11 @@ package com.hdbank.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
